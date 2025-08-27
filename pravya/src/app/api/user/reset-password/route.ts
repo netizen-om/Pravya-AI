62b04150343a05c5495b01a978007b247bf673c2
netizen-om <borisagarom@gmail.com>
2025-08-27 10:48:03 +0530

    added exeception handling in reset-password route

--- pravya/src/app/api/user/reset-password/route.ts
+++ pravya/src/app/api/user/reset-password/route.ts
@@ -5,37 +5,42 @@ import { prisma } from "@/lib/prismadb";
 import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
-  const { oldPassword, newPassword } = await req.json();
-
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.user?.email) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  try {
+    const { oldPassword, newPassword } = await req.json();
+  
+    const session = await getServerSession(authOptions);
+  
+    if (!session || !session.user?.email) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  
+    if (!oldPassword || !newPassword) {
+      return NextResponse.json(
+        { error: "Password is required" },
+        { status: 500 }
+      );
+    }
+  
+    const user = await prisma.user.findUnique({
+      where: { id: session.user.id },
+    });
+    if (!user || !user.password) throw new Error("No user found");
+  
+    const ok = await bcrypt.compare(oldPassword, user.password);
+    if (!ok) throw new Error("Invalid Password");
+  
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+  
+    await prisma.user.update({
+      where: { id: session.user.id },
+      data: {
+        password: newHashedPassword,
+      },
+    });
+  
+    return NextResponse.json({ message : "Password reset successful"}, {status : 200});
+  } catch (error) {
+    return NextResponse.json({ error : "Error resetting Password"}, {status : 500});
+    
   }
-
-  if (!oldPassword || !newPassword) {
-    return NextResponse.json(
-      { error: "Password is required" },
-      { status: 500 }
-    );
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { id: session.user.id },
-  });
-  if (!user || !user.password) throw new Error("No user found");
-
-  const ok = await bcrypt.compare(oldPassword, user.password);
-  if (!ok) throw new Error("Invalid Password");
-
-  const newHashedPassword = await bcrypt.hash(newPassword, 10);
-
-  await prisma.user.update({
-    where: { id: session.user.id },
-    data: {
-      password: newHashedPassword,
-    },
-  });
-
-  return NextResponse.json({ message : "Password reset successful"}, {status : 200});
 }
