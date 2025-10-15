aba4d377843548f9cdeadf6c63757a623e83c233
netizen-om <borisagarom@gmail.com>
2025-10-15 23:25:12 +0530

    interview page partially completed

--- apps/pravya/src/app/interview/page.tsx
+++ apps/pravya/src/app/interview/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
@@ -5,15 +6,29 @@ const page = () => {
     <>
       <div className="min-h-screen w-full bg-zinc-950 flex items-center p-4">
         <div className="bg-zinc-900 w-full">
-          <h2 className="text-2xl px-4 py-2">
+          <h2 className="text-2xl px-4 py-2 pt-4">
             Senior IT Engineer - Live Interview
           </h2>
           <p className="text-l px-4 py-2">Al Interview session for Fun Ltd</p>
 
           <div className="grid grid-cols-3 gap-3 w-full py-2">
-            <div className="w-auto h-[450] bg-blue-500 rounded-lg"></div>
-            <div className="w-auto h-[450] bg-green-500 rounded-lg"></div>
-            <div className="w-auto h-[450] bg-red-500 rounded-lg"></div>
+            <div className="w-auto h-[450] bg-zinc-800 rounded-lg">
+                <div className="flex justify-center items-center h-full w-full">
+                    <div className="w-[110] h-[110] rounded-full bg-white"></div>
+                </div>
+            </div>
+            <div className="w-auto h-[450] bg-zinc-800 rounded-lg">
+                <div className="flex justify-center items-center h-full w-full">
+                    <Image 
+                    src="/user-avatar.png" 
+                    width={110} 
+                    height={110} 
+                    alt="placerholder"
+                    className=" rounded-full object-cover"
+                    />
+                </div>
+            </div>
+            <div className="w-auto h-[450] bg-zinc-800 rounded-lg"></div>
           </div>
         </div>
       </div>
