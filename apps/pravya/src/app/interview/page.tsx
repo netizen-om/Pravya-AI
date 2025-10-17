4c7255a4a18ad61e04ce6e74cd77d1ded82f3c2d
netizen-om <borisagarom@gmail.com>
2025-10-17 11:27:51 +0530

    updated prisma schema

--- apps/pravya/src/app/interview/page.tsx
+++ apps/pravya/src/app/interview/page.tsx
@@ -265,7 +265,7 @@ const page = () => {
   return (
     <>
       <div className="min-h-screen w-full bg-zinc-950 flex items-center p-4">
-        <div className="bg-zinc-900 w-full flex flex-col">
+        <div className="bg-zinc-900 border border-neutral-800 p-2 rou w-full flex flex-col">
           <div>
             <h2 className="text-2xl px-4 py-2 pt-4">
               Senior IT Engineer - Live Interview
