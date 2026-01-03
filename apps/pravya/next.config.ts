33dc0f01dd398ef9dc4e65be96fcca6d932203fb
netizen-om <borisagarom@gmail.com>
2026-01-03 22:29:03 +0530

    trying to fix build error in pravya app

--- apps/pravya/next.config.ts
+++ apps/pravya/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: false,
+  transpilePackages : ["@repo/db", "@repo/auth"],
   devIndicators: false,
   eslint : {
     ignoreDuringBuilds : true
