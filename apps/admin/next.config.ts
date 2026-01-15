6ae2767011a173f61264686c5635da237f898eaa
netizen-om <borisagarom@gmail.com>
2026-01-15 12:40:02 +0530

    changed config for admin app for vercel deployment

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
   reactStrictMode: false,
   transpilePackages : ["@repo/db"],
   devIndicators: false,
@@ -19,7 +18,7 @@ const nextConfig: NextConfig = {
     },
   },
   images: {
-    unoptimized : true,
+    unoptimized : false,
     remotePatterns : [
       {
         protocol: "https",
